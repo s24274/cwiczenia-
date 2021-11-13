acee7f816bebd976aba7f5dda038d80c3486e644
s24274 <91957039+s24274@users.noreply.github.com>
2021-11-13 15:26:45 +0100

    Add files via upload

---
+++ 20014_zadanie.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+
+auto main(int argc, char* argv[]) -> int
+
+{
+	if (argc < 3)
+	{
+	return 1;
+	}
+	
+		auto const wymiar = std::stoi(argv[1]);
+		
+		for (int i = 1; i <= wymiar; i++)
+		{
+			std::cout << "*";
+		}
+		std::cout << "\n";
+		
+    
+		for (int i = 1; i <= wymiar - 2; i++)
+			{
+
+				for (int j = 1; j <= wymiar; j++)
+				{
+				std::cout << " ";
+				}
+				std::cout << "*" << "\n";
+				
+			}
+		for (int k = 1; k <= wymiar; k++)
+		{
+			std::cout << "*";
+		}
+		std::cout << "\n";
+	}
+
+return 0;
+}
+
+
